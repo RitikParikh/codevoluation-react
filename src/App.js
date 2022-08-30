aaa66d827ee1ffd47f608d0f7f5cc2c6d82bd1d4
RitikParikh <ritik.parikh@gig4ce.com>
2022-08-30 11:16:04 +0530

    Ritik:Tut-6

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import React, { Component } from 'react';
 import Great from './components/Great';
+import Welcome from './components/Welcome'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Great/>
+        <Welcome/>
       </div>
     );
   }
