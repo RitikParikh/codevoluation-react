3961ead30947831dd8a5ccbc93f9283a5631c05f
RitikParikh <ritik.parikh@gig4ce.com>
2022-08-30 23:35:14 +0530

    RItik:tut-10

--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,22 @@
 // import logo from '../logo.svg';
 import '../App.css';
 import React, { Component } from 'react';
-import Great from './Great';
-import Welcome from './Welcome'
+// import Great from './Great';
+// import Welcome from './Welcome'
 // import Hello from './Hello';
+import Message from './Message'
 
 class Basic extends Component {
   render() {
     return (
       <div className="App">
-        <Great name = "Dev" heroName="Hello"><p>HI</p></Great>
-        <Great name = "Loooooop" heroName = "OMMMM"/>
-        <Great name = "Orign" heroName = "qaz"/>
-        <Welcome name = "Dev" heroName="Hello"/>
-        <Welcome name = "Loooooop" heroName = "OMMMM"/>
+        {/* <Great name = "Dev" heroName="Hello"><p>HI</p></Great> */}
+        {/* <Great name = "Loooooop" heroName = "OMMMM"/> */}
+        {/* <Great name = "Orign" heroName = "qaz"/> */}
+        {/* <Welcome name = "Dev" heroName="Hello"/> */}
+        {/* <Welcome name = "Loooooop" heroName = "OMMMM"/> */}
         {/* <Hello/> */}
+        <Message/>
       </div>
     );
   }
