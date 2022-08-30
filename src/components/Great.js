3d7d72e37aef1a514983e24637c5c5034a19f8d5
RitikParikh <ritik.parikh@gig4ce.com>
2022-08-30 23:14:49 +0530

    RItik:tut-9

--- src/components/Great.js
+++ src/components/Great.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
-const Great = (prop) =>  <h1>Hello {prop.name}</h1>;
+const Great = (props) => {
+    return (
+        <div>
+            <h1>Hello {props.name}</h1>
+            {props.children}
+        </div>
+    );
+}    
 
 export default Great
--- src/components/index.js
+++ src/components/index.js
@@ -1,18 +1,19 @@
-// import logo from './logo.svg';
+// import logo from '../logo.svg';
 import '../App.css';
 import React, { Component } from 'react';
 import Great from './Great';
-// import Welcome from './Welcome'
+import Welcome from './Welcome'
 // import Hello from './Hello';
 
 class Basic extends Component {
   render() {
     return (
       <div className="App">
-        <Great name = "Dev" heroName="Hello"/>
+        <Great name = "Dev" heroName="Hello"><p>HI</p></Great>
         <Great name = "Loooooop" heroName = "OMMMM"/>
         <Great name = "Orign" heroName = "qaz"/>
-        {/* <Welcome/> */}
+        <Welcome name = "Dev" heroName="Hello"/>
+        <Welcome name = "Loooooop" heroName = "OMMMM"/>
         {/* <Hello/> */}
       </div>
     );
